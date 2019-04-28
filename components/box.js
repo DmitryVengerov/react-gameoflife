3e6595f17d548c625e11693f3daeb38355a3fd9f
Vengerov <hardbeat34@gmail.com>
2019-04-29 00:37:34 +0300

    master-branch add components

---
+++ components/box.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { ButtonToolbar, MenuItem, DropdownButton } from 'react-bootstrap';
+
+class Box extends React.Component {
+	selectBox = () => {
+		this.props.selectBox(this.props.row, this.props.col);
+	}
+
+	render() {
+		return (
+			<div
+				className={this.props.boxClass}
+				id={this.props.id}
+				onClick={this.selectBox}
+			/>
+		);
+	}
+}
